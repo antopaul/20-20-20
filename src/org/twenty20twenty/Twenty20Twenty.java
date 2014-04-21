8ecf208409c59fc8b8a3ea6492bf5b377badbc77
antopaul@gmail.com <antopaul@gmail.com>
2014-04-21 22:20:19 +0530

    Formatted code, added icon to About dialog box.

--- src/org/twenty20twenty/Twenty20Twenty.java
+++ src/org/twenty20twenty/Twenty20Twenty.java
@@ -1,5 +1,4 @@
-
-package  org.twenty20twenty;
+package org.twenty20twenty;
 
 import java.awt.AWTException;
 import java.awt.Image;
@@ -13,6 +12,7 @@ import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
@@ -20,120 +20,129 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class Twenty20Twenty {
-	
+
 	static MenuItem reminderItem = new MenuItem("20-20-20");
-	
+
 	static TrayIcon trayIcon = null;
-	
+
 	static TimerTask reminder = null;
-    static Timer timer = null;
-    
-    static long interval = 20 * 1000 * 60;
-	
-    public static void main(String[] args) {
-        /* Use an appropriate Look and Feel */
-        try {
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-        } catch (UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        } catch (IllegalAccessException ex) {
-            ex.printStackTrace();
-        } catch (InstantiationException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        /* Turn off metal's use of bold fonts */
-        UIManager.put("swing.boldMetal", Boolean.FALSE);
-        //Schedule a job for the event-dispatching thread:
-        //adding TrayIcon.
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-        setTimer();
-    }
-    
-    private static void createAndShowGUI() {
-        //Check the SystemTray support
-        if (!SystemTray.isSupported()) {
-            System.out.println("SystemTray is not supported");
-            return;
-        }
-        final PopupMenu popup = new PopupMenu();
-        trayIcon = new TrayIcon(createImage("/images/eye.gif", "tray icon"));
-        final SystemTray tray = SystemTray.getSystemTray();
-        
-        // Create a popup menu components
-        MenuItem aboutItem = new MenuItem("About");
+	static Timer timer = null;
 
-        MenuItem exitItem = new MenuItem("Exit");
-        
-        //Add components to popup menu
-        popup.add(aboutItem);
-        popup.addSeparator();
-        popup.add(exitItem);
-        
-        trayIcon.setPopupMenu(popup);
-        
-        trayIcon.setToolTip("20-20-20 rule for the eye");
-        
-        try {
-            tray.add(trayIcon);
-        } catch (AWTException e) {
-            System.out.println("TrayIcon could not be added.");
-            return;
-        }
-        
-        aboutItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "20-20-20 rule " +
-                		"for the eye\r\n" +
-                		"_____________________________________________________________________________\r\n" +
-                        "Follow this rule to reduce eye strain when working in front of a computer.\r\n" +
-                        "Every 20 minutes look at something 20 feet away for " +
-                        "20 seconds.\r\n " +
-                        "_____________________________________________________________________________" +
-                        
-                        "\r\nAnto Paul", "About 20-20-20",
-                        JOptionPane.INFORMATION_MESSAGE);
-            }
-        });
-        
-        exitItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tray.remove(trayIcon);
-                System.exit(0);
-            }
-        });
-    }
-    
-    //Obtain the image URL
-    protected static Image createImage(String path, String description) {
-        URL imageURL = Twenty20Twenty.class.getResource(path);
-        
-        if (imageURL == null) {
-            System.err.println("Resource not found: " + path);
-            return null;
-        } else {
-            return (new ImageIcon(imageURL, description)).getImage();
-        }
-    }
+	static long interval = 20 * 1000 * 60;
 
-    protected static void setTimer() {
-    	
-    	reminder = new TimerTask() {
+	public static void main(String[] args) {
+		/* Use an appropriate Look and Feel */
+		try {
+			//UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+		} catch (UnsupportedLookAndFeelException ex) {
+			ex.printStackTrace();
+		} catch (IllegalAccessException ex) {
+			ex.printStackTrace();
+		} catch (InstantiationException ex) {
+			ex.printStackTrace();
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		/* Turn off metal's use of bold fonts */
+		UIManager.put("swing.boldMetal", Boolean.FALSE);
+		// Schedule a job for the event-dispatching thread:
+		// adding TrayIcon.
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+		setTimer();
+	}
+
+	private static void createAndShowGUI() {
+		// Check the SystemTray support
+		if (!SystemTray.isSupported()) {
+			System.out.println("SystemTray is not supported");
+			return;
+		}
+		final PopupMenu popup = new PopupMenu();
+		final Image image = createImage("/images/eye.gif", "tray icon");
+		trayIcon = new TrayIcon(image);
+		final SystemTray tray = SystemTray.getSystemTray();
+
+		// Create a popup menu components
+		MenuItem aboutItem = new MenuItem("About");
+
+		MenuItem exitItem = new MenuItem("Exit");
+
+		// Add components to popup menu
+		popup.add(aboutItem);
+		popup.addSeparator();
+		popup.add(exitItem);
+
+		trayIcon.setPopupMenu(popup);
+
+		trayIcon.setToolTip("20-20-20 Rule For The Eye");
+
+		try {
+			tray.add(trayIcon);
+		} catch (AWTException e) {
+			System.out.println("TrayIcon could not be added.");
+			return;
+		}
+
+		aboutItem.addActionListener(new ActionListener() {
+			
+			
+			public void actionPerformed(ActionEvent e) {
+				Icon icon = new ImageIcon(image);
+				
+				JOptionPane
+						.showMessageDialog(
+								null,
+								"20-20-20 Rule "
+										+ "For The Eye\r\n"
+										+ "______________________________________________________________\r\n"
+										+ "Follow this rule to reduce eye strain when working in front of a computer.\r\n"
+										+ "Every 20 minutes look at something 20 feet away for "
+										+ "20 seconds.\r\n"
+										+ "______________________________________________________________"
+										+
+
+										"\r\nAnto Paul", "About 20-20-20",
+								JOptionPane.PLAIN_MESSAGE, icon);
+			}
+		});
+
+		exitItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tray.remove(trayIcon);
+				System.exit(0);
+			}
+		});
+	}
+
+	// Obtain the image URL
+	protected static Image createImage(String path, String description) {
+		URL imageURL = Twenty20Twenty.class.getResource(path);
+
+		if (imageURL == null) {
+			System.err.println("Resource not found: " + path);
+			return null;
+		} else {
+			return (new ImageIcon(imageURL, description)).getImage();
+		}
+	}
+
+	protected static void setTimer() {
+
+		reminder = new TimerTask() {
 
 			@Override
 			public void run() {
-				trayIcon.displayMessage("20-20-20",
-                        "It's 20-20-20 time", TrayIcon.MessageType.INFO);
+				trayIcon.displayMessage("20-20-20", "It's 20-20-20 time",
+						TrayIcon.MessageType.INFO);
 			}
-    	};
-    	
-    	timer = new Timer();
-    	timer.schedule(reminder, interval, interval);
-    }
+		};
+
+		timer = new Timer();
+		timer.schedule(reminder, interval, interval);
+	}
 }
